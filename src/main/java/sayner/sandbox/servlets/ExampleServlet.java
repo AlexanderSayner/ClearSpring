8717bd3137c641eaeb77cbdcf42a73543a5905ea
Alexander Sayner <alexandr.DEF@mail.ru>
2019-09-12 13:32:46 +0400

    Servlet App

---
+++ src/main/java/sayner/sandbox/servlets/ExampleServlet.java
@@ -0,0 +1,47 @@
+package sayner.sandbox.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ExampleServlet
+ */
+@WebServlet(description = "Сервлет для примера", urlPatterns = { "/ExampleServlet" })
+public class ExampleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ExampleServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String path = request.getContextPath();
+		
+		if (path.equals("")) {
+			path = "/ExampleServlet";
+		}
+
+		response.getWriter().append("Served at: ").append(path);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/java/sayner/sandbox/servlets/HelloServlet.java
@@ -0,0 +1,48 @@
+package sayner.sandbox.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+//@WebServlet(description = "root servlet", urlPatterns = { "/HelloServlet" })
+public class HelloServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * Обработка get запроса
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String path = request.getContextPath();
+		
+		if (path.equals("")) {
+			path = "/HelloServlet";
+		}
+
+		response.getWriter().append("Served at: ").append(path);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
